7ff5bb26461aeb04727bd07d985d63ee8dceab89
Adrián Flores <flo21500@uvg.edu.gt>
2024-08-20 01:08:30 -0600

    feat: SDL Window & FPS Counter

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <SDL2/SDL.h>
+#include <glm/glm.hpp>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+#include "color.h"
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+SDL_Renderer* renderer;
+
+void point(glm::vec2 position, Color color) {
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+    SDL_RenderDrawPoint(renderer, position.x, position.y);
+}
+
+void render() {
+    for (int y = 0; y < SCREEN_HEIGHT; y++) {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            point(glm::vec2(x, y), Color(255,255,0));
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("FPS: 0", 
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+                                          SCREEN_WIDTH, SCREEN_HEIGHT, 
+                                          SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        SDL_Log("Unable to create window: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create a renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (!renderer) {
+        SDL_Log("Unable to create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    int frameCount = 0;
+    Uint32 startTime = SDL_GetTicks();
+    Uint32 currentTime = startTime;
+    
+    while (running) {
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        // Clear the screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        render();
+
+        // Present the renderer
+        SDL_RenderPresent(renderer);
+
+        frameCount++;
+
+        // Calculate and display FPS
+        if (SDL_GetTicks() - currentTime >= 1000) {
+            currentTime = SDL_GetTicks();
+            std::string title = "FPS: " + std::to_string(frameCount);
+            SDL_SetWindowTitle(window, title.c_str());
+            frameCount = 0;
+        }
+    }
+
+    // Cleanup
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
+
